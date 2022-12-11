de24c300273f03bbcb94ea07779c82c0524de323
aHihi <57868058+louisbui@users.noreply.github.com>
2022-12-11 10:25:54 +0700

    update

---
+++ Travel1/Areas/Admin/Controllers/LogginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Travel1.Areas.Admin.Controllers
+{
+    public class LogginController : Controller
+    {
+        // GET: Admin/Loggin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
